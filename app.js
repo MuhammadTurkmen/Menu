1f1319e0cacdf8841d39d928727492c46cf5d99e
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-01-09 10:37:11 +0430

    init

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ const filterBtns = document.querySelectorAll('.filter-btn')
 // load items  
 window.addEventListener('DOMContentLoaded', function() {        
   displayMenuItem(menu)       
-  const categories = menu.reduce(function() {
+  const categories = menu.reduce(function(values) {
 
   }, ['all'])
 })    
