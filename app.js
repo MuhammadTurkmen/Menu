7be32a62924e9a12689d2997f38383e25446f9e4
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-01-09 10:16:11 +0430

    init

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ const menu = [
     title: "steak dinner",
     category: "dinner",
     price: 39.99,
-    img: "./images/item-9.jpeg",
+    img: "./images/item-10.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   }
   
