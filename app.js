42a4737c05f1ede1a8436a2c63b171654f06b804
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-01-12 15:34:08 +0430

    init

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ filterBtns.forEach(function(btn) {
       if(menuItem.category === category) {
         return menuItem  
       } 
-    })   
+    })    
     if(category == 'all') {      
       displayMenuItem(menu)  
     }   
