e452127747cf5f731a72dfa8c4351704a7f6b52d
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-01-29 12:13:53 +0430

    init

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ window.addEventListener('DOMContentLoaded', function() {
     
 filterBtns.forEach(function(btn) {  
   btn.addEventListener('click', function(e) {    
-    console.log(e.currentTarget.dataset.id) 
+    const category = e.currentTarget.dataset.id 
   
   })  
 })
