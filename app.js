f1ee9c8c59e2334468e379141472a9581005207c
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-02 10:25:33 +0430

    init

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ window.addEventListener('DOMContentLoaded', function() {
 // filter items 
    
 filterBtns.forEach(function(btn) {
-  btn.addEventListener('click', function(e) { 
+  btn.addEventListener('click', function(e) {  
 
   })  
 })
