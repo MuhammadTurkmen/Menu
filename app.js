b55540be7f82f04ea2c2c241eb8b8f0193c0e822
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-10 09:53:29 +0430

    init

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ const sectionCenter = document.querySelector('.section-center')
 const filterBtns =
 
  
-
+  
 window.addEventListener('DOMContentLoaded', function() {
   displayMenuItem(menu)
 })
