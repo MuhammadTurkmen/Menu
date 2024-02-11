551a8186f0b263ed3392dc82577d70ba6c4b40b6
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-11 08:42:44 +0430

    init

--- app.js
+++ app.js
@@ -89,6 +89,33 @@ const container = document.querySelector('.btn-container')
 // load items  
 window.addEventListener('DOMContentLoaded', function() {        
   displayMenuItem(menu)       
+})    
+
+
+
+ 
+
+function displayMenuItem(menuItems) { 
+  let displayMenu = menuItems.map(function(item) {
+
+    return `<article class="menu-item">
+          <img src=${item.img} class="photo" alt=${item.title}>
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">$${item.price}</h4>
+            </header>
+            <p class="item-text">
+              ${item.desc}
+            </p>
+          </div>
+        </article>`
+  })
+  displayMenu = displayMenu.join("")
+  sectionCenter.innerHTML = displayMenu
+} 
+
+function displayMenuButtons() {
   const categories = menu.reduce(
     function(values, item) {
       if(!values.includes(item.category)) {
@@ -122,34 +149,6 @@ window.addEventListener('DOMContentLoaded', function() {
     }  // filter items     
 
     
-  })     
-}) 
-})    
-
-
-
- 
-
-function displayMenuItem(menuItems) { 
-  let displayMenu = menuItems.map(function(item) {
-
-    return `<article class="menu-item">
-          <img src=${item.img} class="photo" alt=${item.title}>
-          <div class="item-info">
-            <header>
-              <h4>${item.title}</h4>
-              <h4 class="price">$${item.price}</h4>
-            </header>
-            <p class="item-text">
-              ${item.desc}
-            </p>
-          </div>
-        </article>`
-  })
-  displayMenu = displayMenu.join("")
-  sectionCenter.innerHTML = displayMenu
-} 
-
-function displayMenuButtons() {
-  
+    })     
+  }) 
 }
